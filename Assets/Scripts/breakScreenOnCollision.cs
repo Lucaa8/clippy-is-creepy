4625b50d377c1db74d90bff1816cbffe4310b4da
Luca <luca.dicosola@he-arc.ch>
2023-04-22 13:13:49 +0200

    added clippy animation on scene 1. Removed useless Scritp

--- Assets/Scripts/breakScreenOnCollision.cs
+++ Assets/Scripts/breakScreenOnCollision.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class breakScreenOnCollision : MonoBehaviour
 {
     Renderer image;
+    private bool activeClippy = false;
+
+    public GameObject clippy;
+    public GameObject bubble;
+    public TextMeshPro text;
+    public float MoveSpeed;
+
+    private bool shown = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +24,27 @@ public class breakScreenOnCollision : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       // nothing 
+       if(activeClippy)
+        {
+            if (clippy.transform.position.x > 5)
+            {
+                clippy.transform.position -= clippy.transform.right * MoveSpeed * 10 * Time.deltaTime;
+            }
+            else
+            {
+                if (!shown)
+                {
+                    bubble.SetActive(true);
+                    text.gameObject.SetActive(true);
+                    shown = true;
+                }
+            }
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         image.enabled = true;
+        activeClippy = true;
     }
 }
