d81b0b403e8d25ce549545b92079d8921187f309
unknown <allan.grandjean@he-arc.ch>
2023-04-22 08:35:15 +0200

    Bugs can now be dragged and dropped. Next : throwing them in the bin

---
+++ Assets/Scripts/BugDragnDrop.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugDragnDrop : MonoBehaviour
+{
+    private bool isDragged = false;
+    private Vector3 offset;
+
+
+    void Update()
+    {
+        if (isDragged)
+        {
+            transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        isDragged = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isDragged = false;
+    }
+}
