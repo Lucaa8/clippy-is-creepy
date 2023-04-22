14b7ab6352c166eb14bdf85519fa5c47403f208f
Allan Grandjean <allan.grandjean@he-arc.ch>
2023-04-23 00:27:59 +0200

    THE PLAYER NOW FOLLOWS THE MOUSE CURSOR

--- Assets/Scripts/PlayerMouvement.cs
+++ Assets/Scripts/PlayerMouvement.cs
@@ -15,6 +15,11 @@ public class PlayerMouvement : MonoBehaviour
         mouvement.x = Input.GetAxisRaw("Horizontal");
         mouvement.y = Input.GetAxisRaw("Vertical");
 
+        float h = Input.mousePosition.x - Screen.width / 2;
+        float v = Input.mousePosition.y - Screen.height / 2;
+        float angle = -Mathf.Atan2(v, h) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0, 0, -angle);
     }
 
     private void FixedUpdate()
